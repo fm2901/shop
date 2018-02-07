529a9a4cf95498603f20ecc1a8f693bafb169bac
fm2901 <36166224+fm2901@users.noreply.github.com>
2018-02-07 11:44:55 +0500

    update index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="css/datepicker.css">
 <script src="js/jquery.js"></script>
 <script src="js/script.js"></script>
-<h1><a href="index.php?c=prihod">Приход</a></h1>
+<h1><a href="index.php?c=prihod">Приходs</a></h1>
 <?php
 	include("php/db.php");
 	include("php/tools.php");
